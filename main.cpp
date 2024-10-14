5c9802ccde831722060a9fdee24af701a7797367
WatashiwaLiusan <haiqushi@gmail.com>
2024-10-14 16:06:18 +0200

    Hi

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
 //
 // Created by LiuProJiaJie on 2024/10/14.
 //
+#include "include/raylib.h"
+
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Raylib basic window");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("It works!", 20, 20, 20, BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
